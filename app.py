eb675f2a91b268bd7b392a626429e31af208566f
vijay <jagtapvijay006@gmail.com>
2022-09-12 16:02:42 +0530

    app.py comment add

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from GD import creat_app
 config_name=os.getenv("flask_config","development")
 application=creat_app(config_name)
 
-#demo
+# Hi this is vijay add pull request
 
 if __name__=="__main__":
     application.run(debug=True,port=2000)
